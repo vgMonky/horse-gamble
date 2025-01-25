71bde18f9a04e0f09b391b512d79bf00662ce05a
Victor Gou <victorgou.design@gmail.com>
2025-01-25 14:49:41 -0300

    style enhancment

--- src/app/components/nav-bar.component.ts
+++ src/app/components/nav-bar.component.ts
@@ -25,27 +25,28 @@ import { CommonModule } from '@angular/common';
   // Inline styles
   styles: [`
     nav {
-      padding: 2rem;
+      padding: 2rem 1rem;
       display: flex;
       justify-content: space-between;
+      background-color: var(--c1);
     }
     a {
       text-decoration: none;
       margin: 10px;
-      color: hsl(var(--h), var(--s0), var(--l4))
+      color: var(--c4);
     }
     a:hover {
-      color: hsl(var(--h), var(--s0), var(--l6))
+      color: var(--c5)
     }
     .btn{
       font-size: 10pt;
       padding: 10px 20px;
-      border: 1px solid hsl(var(--h), var(--s5), var(--l2));
+      border: 1px solid var(--c3);
       border-radius: 20px;
       transition: 0.6s;
     }
     .btn:hover{
-      background-color: hsl(var(--h), var(--s4), var(--l2));
+      background-color: var(--c2);
       cursor: pointer;
     }
   `]
