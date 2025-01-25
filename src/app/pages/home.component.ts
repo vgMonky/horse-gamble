62c5beaba31f2eff639ff4019dd569b9445fa683
Victor Gou <victorgou.design@gmail.com>
2025-01-25 17:23:44 -0300

    RXjs component testing

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {ExampleRxComponent} from '../components/example-rx/example-rx.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
+  imports: [ExampleRxComponent],
   template: `
     <h2>Home Page</h2>
     <p>Welcome to the home page!</p>
+    <br>
+    
+    <app-example-rx></app-example-rx>
   `,
   styles: [`
 
