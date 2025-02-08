fd168687738109b7db2bd13c26c4ec5a27a45c94
Victor Gou <victorgou.design@gmail.com>
2025-02-08 06:09:17 -0300

    fixed indentation 4 spaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ import { TradeComponent } from '@app/pages/trade/trade.component';
 import { ExploreComponent } from '@app/pages/explore/explore.component';
 import { PoolComponent } from '@app/pages/pool/pool.component';
 
-export const routes: Routes =  [
-  // Navigate to 'home' component by default (empty path)
-  { path: '', component: HomeComponent },
-  
-  { path: 'trade', component: TradeComponent },
-  { path: 'explore', component: ExploreComponent },
-  { path: 'pool', component: PoolComponent },
+export const routes: Routes = [
+    // Navigate to 'home' component by default (empty path)
+    { path: '', component: HomeComponent },
+    
+    { path: 'trade', component: TradeComponent },
+    { path: 'explore', component: ExploreComponent },
+    { path: 'pool', component: PoolComponent },
 ];
