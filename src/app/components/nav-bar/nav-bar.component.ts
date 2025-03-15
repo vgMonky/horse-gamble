4916c9c6cf6fb33c14c57f9fac95a1b088f5b5fe
Victor Gou <victorgou.design@gmail.com>
2025-03-15 14:41:10 -0300

    new mobile nav-bar structure

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -6,7 +6,7 @@ import { user } from '@app/store/user';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { Subject, takeUntil } from 'rxjs';
-import { WindowContainerComponent } from '@app/components/base-components/window-container/window-container.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-nav-bar',
@@ -15,41 +15,43 @@ import { WindowContainerComponent } from '@app/components/base-components/window
         CommonModule,
         RouterModule,
         LoginComponent,
-        WindowContainerComponent
     ],
-
     templateUrl: './nav-bar.component.html',
     styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit, OnDestroy {
-    isMenuOpen = false;
-    isDarkTheme = false; // track current theme locally
+    isDarkTheme = false;
+    isMobileView = false;
     menuId = 'mobile-menu';
 
     private destroy$ = new Subject<void>();
 
     constructor(
         private store: Store<AppState>,
-        @Inject(DOCUMENT) private document: Document
+        @Inject(DOCUMENT) private document: Document,
+        private breakpointObserver: BreakpointObserver
     ) {}
 
     ngOnInit() {
+        // Detect theme preference
         this.store.select(user.selectors.isDarkTheme)
             .pipe(takeUntil(this.destroy$))
             .subscribe((isDark) => {
                 this.isDarkTheme = isDark;
             });
+
+        // Detect viewport size
+        this.breakpointObserver.observe([Breakpoints.Handset])
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(result => {
+                this.isMobileView = result.matches;
+            });
     }
 
-    toggleMenu() {this.isMenuOpen = !this.isMenuOpen;}
-    closeMenu() {this.isMenuOpen = false;}
-
     toggleTheme() {
-        if (this.isDarkTheme) {
-            this.store.dispatch(user.actions.setLight());
-        } else {
-            this.store.dispatch(user.actions.setDark());
-        }
+        this.isDarkTheme
+            ? this.store.dispatch(user.actions.setLight())
+            : this.store.dispatch(user.actions.setDark());
     }
 
     ngOnDestroy() {
