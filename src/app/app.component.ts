b5ec3270df67d795ee3aa42aa9e6d02e2865790d
Victor Gou <victorgou.design@gmail.com>
2025-03-17 16:36:59 -0300

    new side-container base-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideContainerComponent } from '@app/components/base-components/side-container/side-container.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NavBarComponent],
+    imports: [
+        RouterOutlet,
+        NavBarComponent,
+        SideContainerComponent
+    ],
     template: `
         <app-nav-bar></app-nav-bar>
+        <app-side-container id="mobile-side-menu" side="left">
+            <h2>Side Menu</h2>
+            <p>Navigation links or other content can go here.</p>
+        </app-side-container>
         <div class="view">
             <router-outlet></router-outlet>
         </div>
