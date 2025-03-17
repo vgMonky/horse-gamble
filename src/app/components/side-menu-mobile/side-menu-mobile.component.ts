2f6422df45d78577740400ee0672aa0cb0b2a13c
Victor Gou <victorgou.design@gmail.com>
2025-03-17 19:01:01 -0300

    side-menu-mobile placeholder content

--- src/app/components/side-menu-mobile/side-menu-mobile.component.ts
+++ src/app/components/side-menu-mobile/side-menu-mobile.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { SideContainerComponent } from '@app/components/base-components/side-container/side-container.component';
+import { LucideAngularModule, Omega, Users, Wallet, LogOut } from 'lucide-angular';
+import { SideContainerService } from '@app/components/base-components/side-container/side-container.service';
 
 @Component({
-  selector: 'app-side-menu-mobile',
-  imports: [SideContainerComponent],
-  templateUrl: './side-menu-mobile.component.html',
-  styleUrl: './side-menu-mobile.component.scss'
+    selector: 'app-side-menu-mobile',
+    imports: [SideContainerComponent, LucideAngularModule],
+    templateUrl: './side-menu-mobile.component.html',
+    styleUrl: './side-menu-mobile.component.scss'
 })
 export class SideMenuMobileComponent {
+    readonly OmegaIcon = Omega
+    readonly UsersIcon = Users
+    readonly WalletIcon = Wallet
+    readonly LogoutIcon = LogOut
+
+    constructor(
+        private sideContainerService: SideContainerService,
+    ) {}
+
+    close() {
+        this.sideContainerService.close('mobile-side-menu');
+    }
 
 }
