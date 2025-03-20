0b7ef6a427f1927f6751a7592fadb97408f269fe
Victor Gou <victorgou.design@gmail.com>
2025-03-20 17:29:30 -0300

    login component adjustments

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DropDownComponent } from '@app/components/base-components/drop-down/drop-down.component';
 import { RouterModule } from '@angular/router';
 import { SessionService } from '@app/services/session-kit.service';
+import { LucideAngularModule, User, LogIn } from 'lucide-angular';
 
 @Component({
     selector: 'app-login',
@@ -11,11 +12,14 @@ import { SessionService } from '@app/services/session-kit.service';
         CommonModule,
         DropDownComponent,
         RouterModule,
+        LucideAngularModule
     ],
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+    readonly UserIcon = User
+
     constructor(public sessionService: SessionService) {}
 
     async login() {
