16f32baf8fd79d9a810ed414fc2229028a76f9b5
Victor Gou <victorgou.design@gmail.com>
2025-03-20 17:42:41 -0300

    accounts enhanced

--- src/app/pages/accounts/accounts.component.ts
+++ src/app/pages/accounts/accounts.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { LoginComponent } from '@app/components/login/login.component';
+import { SessionService } from '@app/services/session-kit.service';
 
 @Component({
-  selector: 'app-accounts',
-  imports: [LoginComponent],
-  templateUrl: './accounts.component.html',
-  styleUrl: './accounts.component.scss'
+    selector: 'app-accounts',
+    standalone: true,
+    imports: [CommonModule, LoginComponent],
+    templateUrl: './accounts.component.html',
+    styleUrls: ['./accounts.component.scss']
 })
 export class AccountsComponent {
-
+    constructor(public sessionService: SessionService) {}
 }
