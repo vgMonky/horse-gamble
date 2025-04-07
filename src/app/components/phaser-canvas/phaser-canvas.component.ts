4dc06845a89874dd70ddcfc668011414069fbb7d
Victor Gou <victorgou.design@gmail.com>
2025-04-07 15:08:24 -0300

    phaser sprite sheet working

---
+++ src/app/components/phaser-canvas/phaser-canvas.component.ts
@@ -0,0 +1,104 @@
+import {
+    Component,
+    Input,
+    OnInit,
+    OnDestroy,
+    OnChanges,
+    SimpleChanges,
+    ElementRef,
+    ViewChild
+    } from '@angular/core';
+    import Phaser from 'phaser';
+
+    @Component({
+    selector: 'app-phaser-canvas',
+    templateUrl: './phaser-canvas.component.html',
+    styleUrls: ['./phaser-canvas.component.scss'],
+    standalone: true
+    })
+    export class PhaserCanvasComponent implements OnInit, OnDestroy, OnChanges {
+    @Input() useHorse1: boolean = true;
+
+    @ViewChild('phaserContainer', { static: true }) containerRef!: ElementRef;
+
+    private game?: Phaser.Game;
+
+    ngOnInit(): void {
+        this.startGame();
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['useHorse1'] && !changes['useHorse1'].firstChange) {
+            this.restartScene();
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.game?.destroy(true);
+    }
+
+    private startGame(): void {
+        const self = this;
+
+        class MainScene extends Phaser.Scene {
+            private horseSprite?: Phaser.GameObjects.Sprite;
+
+            preload() {
+                this.load.spritesheet('horse1', 'assets/horse-sprite-sheet-1.jpg', {
+                    frameWidth: 430,
+                    frameHeight: 300,
+                });
+                this.load.spritesheet('horse2', 'public/assets/horse-sprite-sheet-2.jpg', {
+                    frameWidth: 400,
+                    frameHeight: 300,
+                });
+            }
+
+            create() {
+                this.horseSprite = this.add.sprite(400, 300, 'horse1');
+
+                this.anims.create({
+                    key: 'run1',
+                    frames: this.anims.generateFrameNumbers('horse1', {
+                        frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                    }),
+                    frameRate: 15,
+                    repeat: -1,
+                });
+
+                this.anims.create({
+                    key: 'run2',
+                    frames: this.anims.generateFrameNumbers('horse2', {
+                        frames: [3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8],
+                    }),
+                    frameRate: 15,
+                    repeat: -1,
+                });
+
+                const initialKey = self.useHorse1 ? 'run1' : 'run2';
+                this.horseSprite.setScale(0.5).play(initialKey);
+            }
+
+            override update() {
+                if (this.horseSprite) {
+                    const desiredKey = self.useHorse1 ? 'run1' : 'run2';
+                    if (this.horseSprite.anims.currentAnim?.key !== desiredKey) {
+                        this.horseSprite.play(desiredKey);
+                    }
+                }
+            }
+        }
+
+        this.game = new Phaser.Game({
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            parent: this.containerRef.nativeElement,
+            scene: [MainScene],
+        });
+    }
+
+    private restartScene(): void {
+        this.game?.scene.start('MainScene');
+    }
+}
--- src/app/pages/race/race.component.ts
+++ src/app/pages/race/race.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
+import { PhaserCanvasComponent } from '@app/components/phaser-canvas/phaser-canvas.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     standalone: true,
     selector: 'app-trade',
-    imports: [SharedModule],
+    imports: [
+        SharedModule,
+        CommonModule,
+        FormsModule,
+        PhaserCanvasComponent
+    ],
     template: `
         <div class="p-trade">
             <div class="p-trade__title">{{ 'PAGES.HORSE-RACE.T1' | translate }}</div>
-            <p class="p-trade__subtitle">{{ 'PAGES.HORSE-RACE.T2' | translate }}</p>
+            <div class="p-trade__title">{{ 'PAGES.HORSE-RACE.T2' | translate }}</div>
+            <br>
+            <label>
+                <input type="checkbox" [(ngModel)]="useHorse1" />
+                Use Horse 1
+            </label>
+
+            <app-phaser-canvas [useHorse1]="useHorse1" />
         </div>
     `,
     styleUrls: ['./race.component.scss']
 })
-export class RaceComponent {}
+export class RaceComponent {
+    useHorse1 = true;
+}
