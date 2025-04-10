f08068fdd78ad26e0a1c13c59ccefe527ec86350
Victor Gou <victorgou.design@gmail.com>
2025-04-10 15:05:42 -0300

    extracted race page content to horse-race-ui

--- src/app/pages/race/race.component.ts
+++ src/app/pages/race/race.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@app/shared/shared.module';
 import { PhaserCanvasComponent } from '@app/components/phaser-canvas/phaser-canvas.component';
+import { HorseRaceUiComponent } from '@app/components/horse-race-ui/horse-race-ui.component';
 import { RaceService } from '@app/services/race-service';
 import { Subscription } from 'rxjs';
 
@@ -13,37 +14,25 @@ import { Subscription } from 'rxjs';
         SharedModule,
         CommonModule,
         FormsModule,
-        PhaserCanvasComponent
+        PhaserCanvasComponent,
+        HorseRaceUiComponent
     ],
     templateUrl: './race.component.html',
     styleUrls: ['./race.component.scss']
 })
 export class RaceComponent implements OnInit, OnDestroy {
     useHorse1 = true;
-    horses: any[] = [];
-    winner: any = null;
-    podium: any[] = [];
-    finalPosition = 0;
 
     private sub = new Subscription();
 
     constructor(private raceService: RaceService) {}
 
     ngOnInit(): void {
-        this.sub.add(this.raceService.horses$.subscribe(h => this.horses = h));
-        this.sub.add(this.raceService.winner$.subscribe(w => this.winner = w));
-        this.sub.add(this.raceService.podium$.subscribe(p => this.podium = p));
-        this.sub.add(this.raceService.finalPosition$.subscribe(fp => this.finalPosition = fp));
-
         this.raceService.startRace();
     }
 
     ngOnDestroy(): void {
         this.sub.unsubscribe();
         this.raceService.stopRace();
-    }
-
-    restartRace(): void {
-        this.raceService.restartRace();
     }
 }
