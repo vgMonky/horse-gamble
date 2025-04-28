64da1ee645325b453530f38e7276c63f0ef225a2
Victor Gou <victorgou.design@gmail.com>
2025-04-28 20:21:52 -0300

    cleaned phaser-canvas file

--- src/app/components/phaser-canvas/phaser-canvas.component.ts
+++ src/app/components/phaser-canvas/phaser-canvas.component.ts
@@ -1,13 +1,10 @@
 import {
     Component,
-    Input,
     OnInit,
     OnDestroy,
-    OnChanges,
-    SimpleChanges,
     ElementRef,
     ViewChild
-    } from '@angular/core';
+} from '@angular/core';
 import Phaser from 'phaser';
 
 @Component({
@@ -16,21 +13,12 @@ import Phaser from 'phaser';
     styleUrls: ['./phaser-canvas.component.scss'],
     standalone: true
 })
-export class PhaserCanvasComponent implements OnInit, OnDestroy, OnChanges {
-    @Input() useHorse1: boolean = true;
-
+export class PhaserCanvasComponent implements OnInit, OnDestroy {
     @ViewChild('phaserContainer', { static: true }) containerRef!: ElementRef;
-
     private game?: Phaser.Game;
 
     ngOnInit(): void {
         this.startGame();
-    }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes['useHorse1'] && !changes['useHorse1'].firstChange) {
-            this.restartScene();
-        }
     }
 
     ngOnDestroy(): void {
@@ -38,80 +26,39 @@ export class PhaserCanvasComponent implements OnInit, OnDestroy, OnChanges {
     }
 
     private startGame(): void {
-        const self = this;
-
-        class MainScene extends Phaser.Scene {
-            private horseSprite?: Phaser.GameObjects.Sprite;
-
-            preload() {
-                this.load.spritesheet('horse0', 'assets/game-img/sprite-sheet/horse-sprite-sheet-0.png', {
-                    frameWidth: 575,
-                    frameHeight: 434,
-                });
-                this.load.spritesheet('horse1', 'assets/game-img/sprite-sheet/horse-sprite-sheet-1.jpg', {
-                    frameWidth: 430,
-                    frameHeight: 300,
-                });
-                this.load.spritesheet('horse2', 'assets/game-img/sprite-sheet/horse-sprite-sheet-2.jpg', {
-                    frameWidth: 400,
-                    frameHeight: 300,
-                });
-            }
-
-            create() {
-                this.horseSprite = this.add.sprite(150, 150, 'horse1');
-
-                this.anims.create({
-                    key: 'run1',
-                    frames: this.anims.generateFrameNumbers('horse0', {
-                        frames: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-                    }),
-                    frameRate: 13,
-                    repeat: -1,
-                });
-
-                // this.anims.create({
-                //     key: 'run1',
-                //     frames: this.anims.generateFrameNumbers('horse1', {
-                //         frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-                //     }),
-                //     frameRate: 15,
-                //     repeat: -1,
-                // });
-
-                this.anims.create({
-                    key: 'run2',
-                    frames: this.anims.generateFrameNumbers('horse2', {
-                        frames: [3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8],
-                    }),
-                    frameRate: 15,
-                    repeat: -1,
-                });
-
-                const initialKey = self.useHorse1 ? 'run1' : 'run2';
-                this.horseSprite.setScale(0.5).play(initialKey);
-            }
-
-            override update() {
-                if (this.horseSprite) {
-                    const desiredKey = self.useHorse1 ? 'run1' : 'run2';
-                    if (this.horseSprite.anims.currentAnim?.key !== desiredKey) {
-                        this.horseSprite.play(desiredKey);
-                    }
-                }
-            }
-        }
-
         this.game = new Phaser.Game({
             type: Phaser.AUTO,
             width: 300,
-            height: 300,
+            height: 200,
             parent: this.containerRef.nativeElement,
             scene: [MainScene],
         });
     }
+}
 
-    private restartScene(): void {
-        this.game?.scene.start('MainScene');
+class MainScene extends Phaser.Scene {
+    private horseSprite!: Phaser.GameObjects.Sprite;
+
+    constructor() {
+        super('MainScene');
+    }
+
+    preload() {
+        this.load.spritesheet(
+            'horse0',
+            'assets/game-img/sprite-sheet/horse-sprite-sheet-0.png',
+            { frameWidth: 575, frameHeight: 434 }
+        );
+    }
+
+    create() {
+        this.horseSprite = this.add.sprite(150, 100, 'horse0');
+        this.anims.create({
+            key: 'run1',
+            frames: this.anims.generateFrameNumbers('horse0', { frames: [0,1,2,3,4,5,6,7,8] }),
+            frameRate: 13,
+            repeat: -1,
+        });
+        this.horseSprite.setScale(0.3).play('run1');
     }
 }
