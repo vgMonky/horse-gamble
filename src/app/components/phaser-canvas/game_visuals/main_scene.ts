e4ab9ed53d762433f4cb769299f88a922732e55b
Victor Gou <victorgou.design@gmail.com>
2025-05-01 11:45:29 -0300

    sky gradient color adjustment

--- src/app/components/phaser-canvas/game_visuals/main_scene.ts
+++ src/app/components/phaser-canvas/game_visuals/main_scene.ts
@@ -26,11 +26,12 @@ export class MainScene extends Phaser.Scene {
 
         // 1️⃣ sky gradient
         const g = this.add.graphics();
-        const topColor    = Phaser.Display.Color.HSLToColor(0.6, 0.3, 0.6).color;
-        const bottomColor = Phaser.Display.Color.HSLToColor(0.5, 0.3, 0.9).color;
-        g.fillGradientStyle(topColor, topColor, bottomColor, bottomColor, 1);
-        g.fillRect(0, 0, width, height);
-        g.setDepth(-7);  // behind your 6 parallax layers
+        const topColor    = Phaser.Display.Color.HSLToColor(0.6, 0.2, 0.6).color;
+        const topColor2    = Phaser.Display.Color.HSLToColor(0.99, 0.2, 0.7).color;
+        const bottomColor = Phaser.Display.Color.HSLToColor(0.5, 0.2, 0.9).color;
+        g.fillGradientStyle(topColor2, topColor, bottomColor, bottomColor, 1);
+        g.fillRect(-100, 0, width + 100, height);
+        g.setDepth(-10);  // behind your 6 parallax layers
 
         // 2️⃣ parallax background
         this.bg.create();
