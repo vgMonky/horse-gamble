4110da8b85e5357432b1588cf8150e8f07cfe63b
Victor Gou <victorgou.design@gmail.com>
2025-05-19 18:05:42 -0300

    remove phaser canvas subscriptions

--- src/app/components/phaser-canvas/phaser-canvas.component.ts
+++ src/app/components/phaser-canvas/phaser-canvas.component.ts
@@ -10,7 +10,7 @@ import Phaser from 'phaser';
 import { MainScene } from './game_visuals/main_scene';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { BREAKPOINT } from 'src/types';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { OngoingRaceService } from '@app/game/ongoing-race.service';
 import { CommonModule } from '@angular/common';
@@ -22,47 +22,51 @@ import { FormsModule } from '@angular/forms';
     styleUrls: ['./phaser-canvas.component.scss'],
     standalone: true,
     imports: [ CommonModule, FormsModule ]
-
 })
 export class PhaserCanvasComponent implements OnInit, OnDestroy {
     @ViewChild('phaserContainer', { static: true })
     containerRef!: ElementRef;
-    isMobileView$: Observable<boolean>;
-    private game?: Phaser.Game;
-    markerOpacity = 1; // default full opacity
-    lightnessValue = 0.1; // default starting value
 
+    isMobileView$: Observable<boolean>;
+    markerOpacity = 1;      // default full opacity
+    lightnessValue = 0.1;   // default starting value
+
+    private game?: Phaser.Game;
+    private gameSubs: Subscription[] = [];
 
     constructor(
         private breakpointObserver: BreakpointObserver,
         private ongoingRaceService: OngoingRaceService
     ) {
         this.isMobileView$ = this.breakpointObserver
-        .observe(BREAKPOINT)
-        .pipe(
-            map(result => result.matches),
-        );
+            .observe(BREAKPOINT)
+            .pipe(map(result => result.matches));
     }
 
     ngOnInit(): void {
-        // boot Phaser
         this.startGame();
     }
 
     ngOnDestroy(): void {
+        // destroy Phaser instance
         this.game?.destroy(true);
     }
 
     private startGame(): void {
-        // pass the race service so MainScene can grab horses$
-        const scene = new MainScene(this.ongoingRaceService, () => this.markerOpacity, () => this.lightnessValue);
+        // instantiate the scene
+        const scene = new MainScene(
+            this.ongoingRaceService,
+            () => this.markerOpacity,
+            () => this.lightnessValue
+        );
 
+        // boot Phaser
         this.game = new Phaser.Game({
             type: Phaser.AUTO,
             width: 1000,
             height: 250,
             parent: this.containerRef.nativeElement,
-            scene: scene
+            scene
         });
     }
 }
