af42a3bb2ceda7d8985b1b4a909058dcfb7c594d
Victor Gou <victorgou.design@gmail.com>
2025-05-26 15:43:47 -0300

    horses named with colors adjusted

--- src/app/game/horses-database.ts
+++ src/app/game/horses-database.ts
@@ -6,12 +6,12 @@ export interface Horse {
 
 export const ALL_HORSES: Horse[] = [
     { index:  1,  name: 'Fast Fury'       },
-    { index:  2,  name: 'Red Rocket'       },
+    { index:  2,  name: 'Rare Rocket'       },
     { index:  3,  name: 'Silver Arrow'     },
     { index:  4,  name: 'Wind Chaser'      },
     { index:  5,  name: 'Storm Bringer'    },
     { index:  6,  name: 'Lightning Bolt'   },
-    { index:  7,  name: 'Golden Hoof'      },
+    { index:  7,  name: 'Tony Hoof'      },
     { index:  8,  name: 'Lucky Star'       },
     { index:  9,  name: 'Shadow Dancer'    },
     { index: 10,  name: 'Thunder Strike'   },
@@ -20,7 +20,7 @@ export const ALL_HORSES: Horse[] = [
     { index: 13,  name: 'Majestic Wind'    },
     { index: 14,  name: 'Night Rider'      },
     { index: 15,  name: 'Crimson Trail'    },
-    { index: 16,  name: 'Emerald Sprint'   },
+    { index: 16,  name: 'Harry Sprint'   },
     { index: 17,  name: 'Sapphire Skies'   },
     { index: 18,  name: 'Onyx Spirit'      },
     { index: 19,  name: 'Diamond Dancer'   },
