a09556dabcf941e43bdabee256559abb0eeb8536
Victor Gou <victorgou.design@gmail.com>
2025-06-11 10:57:20 -0300

    in race page expandable for each race, and expandable group wrapper

--- src/app/pages/races/races/races.component.ts
+++ src/app/pages/races/races/races.component.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@app/shared/shared.module';
 import { OngoingRacesComponent } from '@app/components/ongoing-race/ongoing-race.component';
 import { HorseRaceService } from '@app/game/horse-race.service';
-import { combineLatest } from 'rxjs';
+import { 
+    combineLatest,
+    Subject,
+    takeUntil
+} from 'rxjs';
 import { ExpandableComponent } from '@app/components/base-components/expandable/expandable.component';
 import { ExpandableGroupComponent } from '@app/components/base-components/expandable/expandable-group.component';
 
@@ -14,20 +18,48 @@ import { ExpandableGroupComponent } from '@app/components/base-components/expand
     imports: [
         SharedModule,
         CommonModule,
-        OngoingRacesComponent
+        OngoingRacesComponent,
+        ExpandableComponent,
+        ExpandableGroupComponent
     ],
     templateUrl: './races.component.html',
     styleUrls: ['./races.component.scss']
 })
 export class RacesComponent {
     visibleRaceIds: number[] = [];
+    countdowns: Record<number, number> = {};
+
+    private destroy$ = new Subject<void>();
 
     constructor(private horseRaceService: HorseRaceService) {
         combineLatest([
             this.horseRaceService.getAllRaceIds$(),
             this.horseRaceService.getCompletedRaceIds$()
-        ]).subscribe(([allIds, completed]) => {
-            this.visibleRaceIds = allIds.filter(id => !completed.includes(id));
+        ])
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(([allIds, completed]) => {
+                this.visibleRaceIds = allIds.filter(id => !completed.includes(id));
+                this.trackCountdowns(this.visibleRaceIds);
+            });
+    }
+
+    getDistance(raceId: number): number {
+        return this.horseRaceService.manager.getWinningDistance(raceId) / 10;
+    }
+
+    private trackCountdowns(ids: number[]): void {
+        ids.forEach(id => {
+            this.horseRaceService.manager
+                .getCountdown$(id)
+                .pipe(takeUntil(this.destroy$))
+                .subscribe(cnt => {
+                    this.countdowns[id] = cnt;
+                });
         });
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 }
